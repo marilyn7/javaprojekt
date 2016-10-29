4022a885ca008dc8885b361b8317c8e44167904d
marilyn7 <marilyn737@gmail.com>
2016-10-29 10:37:36 +0300

    Add files via upload
    õige projekti algus, käivitamise nupp

--- Main.java
+++ Main.java
@@ -7,73 +7,46 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.shape.Circle;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-/**
- * Created by Marilyn on 25.10.2016.
- */
-public class Main extends Application{
+
+public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Jaapani mõistatus");
+        primaryStage.show();
+
 
         VBox vbox = new VBox();
-        Scene login = new Scene(vbox, 200, 100);
+        Scene login = new Scene(vbox, 300, 250);
         primaryStage.setScene(login);
         primaryStage.show();
 
-        Label pealkiri = new Label("Mis on parool?");
-        TextField paroolField = new TextField();
-        Button submitButton = new Button("Login");
-
-        vbox.getChildren().addAll(pealkiri, paroolField, submitButton);
-
-        Circle ring = new Circle(150);
+        Label pealkiri = new Label("Jaapani mõistatus");
+        Button submitButton = new Button("Alusta!");
         Pane pane = new Pane();
-        ring.setCenterX(175);
-        ring.setCenterY(200);
-        pane.getChildren().add(ring);
-        Scene seesStseen = new Scene(pane, 350, 350);
+        pane.getChildren().add(pealkiri);
+        pealkiri.setTranslateX(100);
+        pealkiri.setTranslateY(75);
+        pane.getChildren().add(submitButton);
+        submitButton.setTranslateX(100);
+        submitButton.setTranslateY(100);
 
-        submitButton.setOnAction(event -> {
-            String parool = paroolField.getText();
-            if (parool.equals("pass")){
-                System.out.println("õige!");
-                primaryStage.setScene(seesStseen);
-            } else {
-                System.out.println("proovi uuesti!");
-            }
+
+        vbox.getChildren().addAll(pealkiri, submitButton);
+
+        submitButton.setText("Alusta mängu");
+
+        StackPane stack = new StackPane();
+        GridPane grid = new GridPane();
+        grid.setGridLinesVisible(true);
+        Scene mõistatus = new Scene(stack, 450, 550);
+
+
+        submitButton.setOnAction((event) -> {
+            primaryStage.setScene(mõistatus);
+
 
         });
-
-
-
-
-        /*Pane pane = new Pane();
-        Scene scene = new Scene(pane, 350, 350);
-        primaryStage.setScene(scene);
-
-        primaryStage.show();
-
-        /*System.out.println("Tere tulemast mängu!");*/
-
-        /*GridPane grid = new GridPane();
-        grid.add(rect, 1, 5);
-        grid.setGridLinesVisible(true);
-        stack.getChildren().add(grid);*/
-
-
-        /*Circle ring = new Circle(100);
-        ring.setCenterX(175);
-        ring.setCenterY(200);
-        pane.getChildren().add(ring);
-
-        ring.setOnMouseClicked(event -> {
-            System.out.println("KLIKK");
-            ring.setCenterY(175);
-
-        });*/
     }
-
 }
